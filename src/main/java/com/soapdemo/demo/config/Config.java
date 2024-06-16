b06ec737e45f5d74c97f6e73f80d817f9f4e2e06
Shrikant Pardhi <shripardhi92@gmail.com>
2024-06-16 16:41:13 +0530

    Create Config.java

---
+++ src/main/java/com/soapdemo/demo/config/Config.java
@@ -0,0 +1,27 @@
+package com.soapdemo.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("com.soapdemo.demo.jaxbmodel");
+        marshaller.setCheckForXmlRootElement(false);
+        return marshaller;
+    }
+
+    @Bean
+    public CountryClient countryClient(Jaxb2Marshaller marshaller) {
+        CountryClient client = new CountryClient();
+        client.setDefaultUri("http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso");
+        client.setMarshaller(marshaller);
+        client.setUnmarshaller(marshaller);
+        return client;
+    }
+
+}
